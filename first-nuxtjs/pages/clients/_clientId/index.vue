e1c881a5156e256df5c9731cfb691220fe4575d8
vudinhquang <vudinhquangk53@gmail.com>
2020-09-29 01:28:09 +0900

    Dynamic Routes

---
+++ first-nuxtjs/pages/clients/_clientId/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Client Page - Id: {{ $route.params.clientId }}</h1>
+</template>
+
+<script>
+export default {
+  mounted () {
+    console.log(this.$route.params.clientId);
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ first-nuxtjs/pages/clients/_clientId/settings.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Settings Page - Id: {{ $route.params.clientId }}</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
