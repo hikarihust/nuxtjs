708dc40b181c19ede12a87865a7ffeed7888590b
vudinhquang <vudinhquangk53@gmail.com>
2020-09-29 10:36:39 +0900

    Nuxt Link

--- first-nuxtjs/pages/users.vue
+++ first-nuxtjs/pages/users.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
+    <ul>
+      <li>
+        <nuxt-link
+          activeClass="active"
+          exactActiveClass="exact-active"
+          to="/users">Users Page</nuxt-link></li>
+      <li>
+        <nuxt-link
+          activeClass="active"
+          exactActiveClass="exact-active"
+          to="/users/20" tag="button">User Detail</nuxt-link></li>
+      <li>
+        <nuxt-link
+          activeClass="active"
+          exactActiveClass="exact-active"
+          to="/users/settings">Users Settings</nuxt-link></li>
+    </ul>
     <h1>Users Page - users.vue</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ipsa beatae molestiae
       quidem facere adipisci, omnis veniam iste similique non obcaecati, quos magnam cum asperiores,
       consequuntur necessitatibus modi cumque! Quae?
     </p>
-    <NuxtChild />
+    <nuxt-child />
   </div>
 </template>
 
@@ -17,5 +34,7 @@ export default {
 </script>
 
 <style>
-
+  .exact-active {
+    color: red;
+  }
 </style>
