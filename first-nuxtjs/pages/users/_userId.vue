d4c8e7eb8d8be331923544895296bbbf0493e034
vudinhquang <vudinhquangk53@gmail.com>
2020-09-29 10:57:46 +0900

    Validate Params

--- first-nuxtjs/pages/users/_userId.vue
+++ first-nuxtjs/pages/users/_userId.vue
@@ -4,6 +4,13 @@
 
 <script>
 export default {
+  validate({ params }) {
+    // '20a' -> NaN -> false -> !false -> true
+    if(!Number(params.userId)) {
+      return false;
+    }
+    return true;
+  }
 }
 </script>
 
