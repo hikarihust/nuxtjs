dc51837293abfda86bca053a9727576a0616b6b3
vudinhquang <vudinhquangk53@gmail.com>
2020-09-29 11:54:48 +0900

    Error Page

---
+++ first-nuxtjs/layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page not  {{ error.statusCode }}</h1>
+    <h1 v-else>An error occurred</h1>
+    <p>{{ error.message }}</p>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
