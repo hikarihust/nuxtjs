4c2b78f5f6c564149916939226b2dff186851793
vudinhquang <vudinhquangk53@gmail.com>
2020-09-30 23:54:41 +0900

    FetchMethod 1

---
+++ first-nuxtjs/pages/fetchMethod.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Demo Fetch Method</h1>
+    <nuxt-link to="/">Trang Chu</nuxt-link>
+    <ul>
+      <li v-for="item in listCategories" v-bind:key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'fetch-data',
+  data() {
+    return {
+      listCategories: []
+    }
+  },
+  async asyncData({ store, $axios }) {
+    // console.log("asyncData run", this);
+  },
+  created() {
+    // console.log("created run", this);
+  },
+  async fetch() {
+    // console.log("fetch run", this);
+    const data = await this.$axios.$get("http://localhost/wp-api-test/wp-json/wp/v2/categories")
+    this.listCategories = data
+  }
+}
+</script>
+
+<style>
+
+</style>
