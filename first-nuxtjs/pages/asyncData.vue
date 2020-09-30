eee8642abc411047a83be3c2cc2909cb30a02732
vudinhquang <vudinhquangk53@gmail.com>
2020-09-30 18:11:34 +0900

    AsyncData 2

--- first-nuxtjs/pages/asyncData.vue
+++ first-nuxtjs/pages/asyncData.vue
@@ -18,6 +18,7 @@ export default {
       listCategories: []
     }
   },
+  /*
   asyncData({$axios}) {
     // console.log(Object.keys(context));
     // console.log($axios);
@@ -28,15 +29,29 @@ export default {
           listCategories: data
         }
       })
-    /*
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          listNumber: [1, 4, 3, 10]
-        })
-      }, 2000);
-    })
-    */
+
+    // return new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve({
+    //       listNumber: [1, 4, 3, 10]
+    //     })
+    //   }, 2000);
+    // })
+  }
+  */
+  async asyncData({ $axios }) {
+      const promiseCategories = $axios.get("http://localhost/wp-api-test/wp-json/wp/v2/categories") // 2
+      const promisePosts = $axios.get("http://localhost/wp-api-test/wp-json/wp/v2/posts") // 3
+
+      const [resPosts, resCategories] = await Promise.all([
+        promisePosts,
+        promiseCategories,
+      ])
+
+      return {
+        listPosts: resPosts.data,
+        listCategories: resCategories.data
+      }
   }
 }
 </script>
