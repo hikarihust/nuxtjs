86fc59bfd05c19587bb5c9fdee003da4f3a5f0de
vudinhquang <vudinhquangk53@gmail.com>
2020-10-01 02:01:03 +0900

    Nuxt Server Init

--- first-nuxtjs/store/index.js
+++ first-nuxtjs/store/index.js
@@ -9,6 +9,10 @@ export const mutations = {
 }
 
 export const actions = {
+  async nuxtServerInit (vuxContext, nuxtContext) {
+    console.log(Object.keys(vuxContext));
+    console.log(Object.keys(nuxtContext));
+  },
   increment (context) {
     context.commit('increment')
   }
