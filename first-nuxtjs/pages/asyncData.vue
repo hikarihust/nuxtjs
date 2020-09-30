821103daef445da96b6075e97734c3f22c30ce2b
vudinhquang <vudinhquangk53@gmail.com>
2020-09-30 17:39:51 +0900

    AsyncData 1

---
+++ first-nuxtjs/pages/asyncData.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Demo AsyncData {{ counter }}</h1>
+    <nuxt-link to="/">Trang Chu</nuxt-link>
+    <ul>
+      <li v-for="item in listCategories" v-bind:key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'fetch-data',
+  data() {
+    return {
+      counter: 10,
+      listPosts: [],
+      listCategories: []
+    }
+  },
+  asyncData({$axios}) {
+    // console.log(Object.keys(context));
+    // console.log($axios);
+    return $axios.$get("http://localhost/wp-api-test/wp-json/wp/v2/categories")
+      .then(data => {
+        console.log(data);
+        return {
+          listCategories: data
+        }
+      })
+    /*
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          listNumber: [1, 4, 3, 10]
+        })
+      }, 2000);
+    })
+    */
+  }
+}
+</script>
+
+<style>
+
+</style>
