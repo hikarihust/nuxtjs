93d9b58f459470571037c36cccd96818df3eaafb
vudinhquang <vudinhquangk53@gmail.com>
2020-10-01 01:00:46 +0900

    FetchMethod 2

---
+++ first-nuxtjs/components/DemoFetchComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Demo Fetch Components - Tags: {{ listTags.length }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listTags: []
+    }
+  },
+  created () {
+    console.log("created demo fetch component run");
+  },
+  async fetch() {
+    console.log("fetch demo fetch component run");
+    const data = await this.$axios.$get("http://localhost/wp-api-test/wp-json/wp/v2/tags")
+    this.listTags = data;
+  }
+}
+</script>
+
+<style>
+
+</style>
--- first-nuxtjs/pages/fetchMethod.vue
+++ first-nuxtjs/pages/fetchMethod.vue
@@ -5,25 +5,30 @@
     <ul>
       <li v-for="item in listCategories" v-bind:key="item.id">{{ item.name }}</li>
     </ul>
+    <DemoFetchComponent />
   </div>
 </template>
 
 <script>
+import DemoFetchComponent from '@/components/DemoFetchComponent';
 export default {
   layout: 'fetch-data',
+  components: {
+    DemoFetchComponent,
+  },
   data() {
     return {
       listCategories: []
     }
   },
   async asyncData({ store, $axios }) {
-    // console.log("asyncData run", this);
+    console.log("asyncData run");
   },
   created() {
-    // console.log("created run", this);
+    console.log("created run");
   },
   async fetch() {
-    // console.log("fetch run", this);
+    console.log("fetch run");
     const data = await this.$axios.$get("http://localhost/wp-api-test/wp-json/wp/v2/categories")
     this.listCategories = data
   }
