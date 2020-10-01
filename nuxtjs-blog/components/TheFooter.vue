8937b123c69495deefe51fb497c152a41b023596
vudinhquang <vudinhquangk53@gmail.com>
2020-10-01 16:00:41 +0900

    Header & Footer

---
+++ nuxtjs-blog/components/TheFooter.vue
@@ -0,0 +1,67 @@
+<template>
+  <footer id="footer" class="bg-white">
+      <div class="tcl-container">
+          <div class="footer">
+              <div class="tcl-row">
+                  <!-- Footer Column -->
+                  <div class="tcl-col-12 tcl-col-sm-6 tcl-col-md-4 tcl-col-lg-3">
+                      <div class="footer-logo">
+                          <img src="/assets/images/logo.png" alt="NuxtBlog Logo">
+                      </div>
+                      <p>© 2020, All Rights Reserved.</p>
+                      <p>Created by <a href="https://www.google.com/" target="_blank">Quang</a></p>
+                  </div>
+                  <!-- Footer Column -->
+                  <div class="tcl-col-12 tcl-col-sm-6 tcl-col-md-4 tcl-col-lg-2">
+                      <div class="footer-title">
+                          <p>Categories</p>
+                      </div>
+                      <ul class="footer-content__list">
+                          <li><a href="#">ReactJs</a></li>
+                          <li><a href="#">Javascript</a></li>
+                          <li><a href="#">Angular</a></li>
+                          <li><a href="#">HTML, HTML5</a></li>
+                      </ul>
+                  </div>
+                  <!-- Footer Column -->
+                  <div class="tcl-col-12 tcl-col-sm-6 tcl-col-md-4 tcl-col-lg-3">
+                      <div class="footer-title">
+                          <p>Liên hệ</p>
+                      </div>
+                      <ul class="footer-content__list">
+                          <li>219/78 Trần Văn Đang - Quận 10 - Thành phố Hồ chí Minh</li>
+                          <li>0343 261 825</li>
+                      </ul>
+                  </div>
+                  <!-- Footer Column -->
+                  <div class="tcl-col-12 tcl-col-sm-6 tcl-col-md-4 tcl-col-lg-4">
+                      <div class="footer-title">
+                          <p>Fanpage</p>
+                      </div>
+                      <div class="footer-facebook">
+                          <div class="fb-page" data-href="https://www.google.com/" data-tabs=""
+                              data-width="" data-height="" data-small-header="false" data-adapt-container-width="true"
+                              data-hide-cover="false" data-show-facepile="true">
+                              <blockquote cite="https://www.google.com/"
+                                  class="fb-xfbml-parse-ignore">
+                                  <a href="https://www.google.com/">Học Lập Trình Web Thông Qua
+                                  Projects Thực Tế</a>
+                              </blockquote>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  @import url("~/assets/css/footer.css");
+</style>
--- nuxtjs-blog/nuxt.config.js
+++ nuxtjs-blog/nuxt.config.js
@@ -8,7 +8,11 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,500;0,600;0,700;1,400&amp;display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&amp;display=swap' },
+      { rel: 'stylesheet', href: 'http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css' },
+      { rel: 'stylesheet', href: '/assets/libs/bootstrap4/bootstrap-tcl.css' }
     ]
   },
 
