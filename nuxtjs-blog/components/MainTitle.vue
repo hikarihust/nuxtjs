c9f16570539ab017f413eea8e5b962ccbf3b9937
vudinhquang <vudinhquangk53@gmail.com>
2020-10-01 22:50:06 +0900

    Latest Articles

---
+++ nuxtjs-blog/components/MainTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-title spacing">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+  },
+}
+</script>
+  @import url('~/assets/css/main-title.css');
+<style>
+</style>
--- nuxtjs-blog/pages/index.vue
+++ nuxtjs-blog/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container">
-    <h1>Demo Apache</h1>
+  <div class="wrapper-content">
+    <NewsLatest />
   </div>
 </template>
 
