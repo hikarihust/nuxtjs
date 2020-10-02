565ee43929d6ed8dc325e49d2f18d20e5050dbbc
vudinhquang <vudinhquangk53@gmail.com>
2020-10-02 16:30:30 +0900

    WP custom thumb 2

--- wp-api/wp-custom-api.php
+++ wp-api/wp-custom-api.php
@@ -2,7 +2,7 @@
   add_action( 'rest_api_init', function () {
     // Get Image Thumbnail by featured_media id
     register_rest_field('post',
-      'featured_media_url', // Vi tri nay tren tuy chon
+      'featured_media_url', // định nghĩa tên trường url của hình ảnh
       array(
         'get_callback'  => 'get_rest_featured_media_url' // Ten ham
       )
@@ -11,10 +11,11 @@
 
   function get_rest_featured_media_url($post, $field_name, $request) {
     $post_id = $post['id'];
+    // $media_id = $post['featured_media'];
 
     if ($post_id) {
       $url = get_the_post_thumbnail_url($post_id);
-      // $arrUrl = wp_get_attachment_image_src($media_id);
+      // $url = wp_get_attachment_image_src($media_id);
       return $url;
     }
 
