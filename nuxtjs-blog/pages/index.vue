4d7ab9c277375b4dedcfb229512ca05121aa0a93
vudinhquang <vudinhquangk53@gmail.com>
2020-10-02 11:36:18 +0900

    API Latest Posts

--- nuxtjs-blog/pages/index.vue
+++ nuxtjs-blog/pages/index.vue
@@ -7,7 +7,11 @@
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ store }) {
+    await store.dispatch('posts/actFetchLatestPost');
+  }
+}
 </script>
 
 <style>
--- nuxtjs-blog/store/posts/actions.js
+++ nuxtjs-blog/store/posts/actions.js
@@ -1,5 +1,26 @@
 export default {
   async actFetchLatestPost({ commit }) {
+    try {
+      const response = await this.$api.get('/postss', {
+        params: {
+          page: 1,
+          per_page: 3
+        }
+      });
 
+      if (response.status === 200) {
+        commit('setLatestList', response.data);
+      }
+      return {
+        ok: true
+      }
+    } catch(e) {
+      console.error("actFetchLatestPost", e.response.data.message);
+      return {
+        ok: false,
+        data: e.response.data,
+        error: e.response.data.message
+      }
+    }
   }
 }
