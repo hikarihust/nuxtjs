928cd2619d2df5a667248de549ce9ecec5d68241
vudinhquang <vudinhquangk53@gmail.com>
2020-10-03 20:09:24 +0900

    Map Menu Items

--- nuxtjs-blog/store/index.js
+++ nuxtjs-blog/store/index.js
@@ -12,6 +12,54 @@ export const mutations = {
   }
 }
 
+
+export const getters = {
+  mainMenuItems(rootState) {
+    const newItems = rootState.appMainMenus.items.map(item => {
+      const data = {
+        id: item.ID,
+        url: item.url,
+        title: item.title,
+      }
+
+      let child_items = item.child_items;
+
+      if (!child_items) {
+        data.child_items = [];
+      } else {
+        data.child_items = child_items.map(_item => {
+          const _data = {
+            id: _item.ID,
+            url: _item.url,
+            title: _item.title,
+          }
+
+          let _child_items = _item.child_items;
+          if (!_child_items) {
+            _data.child_items = [];
+          } else {
+            _data.child_items = _child_items.map(__item => {
+              const __data = {
+                id: __item.ID,
+                url: __item.url,
+                title: __item.title,
+              }
+
+              return __data;
+            })
+          }
+
+          return _data;
+        })
+      }
+      return data;
+    })
+    // console.log("newItems", newItems);
+    return newItems;
+  }
+}
+
+
 export const actions = {
   async nuxtServerInit({ dispatch }) {
     await Promise.all([
