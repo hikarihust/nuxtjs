c04bb763cfb01264175ebe98e8ceaefc8a0a5917
vudinhquang <vudinhquangk53@gmail.com>
2020-10-04 02:13:09 +0900

    Search Page

---
+++ nuxtjs-blog/pages/search.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="articles-list section">
+    <div class="tcl-container">
+      <div class="main-title main-title__search spacing">
+        <h2>4 Results found for "search query"</h2>
+      </div>
+
+      <!-- Article List -->
+      <div class="tcl-row tcl-jc-center">
+        <div class="tcl-col-12 tcl-col-md-8">
+          <ArticleItem
+            isShowDesc
+            isShowCategories
+            :isStyleRow="isStyleRow"
+            :isStyleCard="isStyleCard"
+          />
+        </div>
+      </div>
+
+      <div class="text-center">
+        <AppButton
+          isSizeLarge
+          type="primary"
+          :isLoading="true"
+          v-on:click.native="()=>{}"
+        >Tải thêm</AppButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
