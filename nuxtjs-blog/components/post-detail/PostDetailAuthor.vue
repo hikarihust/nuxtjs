56eb158415041fe47bd01c5091752034e7cd033f
vudinhquang <vudinhquangk53@gmail.com>
2020-11-19 01:06:42 +0900

    Render detail 3

--- nuxtjs-blog/components/post-detail/PostDetailAuthor.vue
+++ nuxtjs-blog/components/post-detail/PostDetailAuthor.vue
@@ -1,21 +1,40 @@
 <template>
-  <div class="post-author">
+  <div class="post-author" v-if="post">
     <div class="post-author__bg-avatar">
-      <a href="#" class="post-author__avatar">
-        <img src="/assets/images/blog-detail.jpg" alt="">
-      </a>
+      <nuxt-link v-bind:to="authorLink" class="post-author__avatar">
+        <img
+          v-bind:src="getAvatar"
+          v-bind:alt="post.author_data.nickname"
+        />
+      </nuxt-link>
     </div>
     <div class="post-author__nickname">
-      <a href="#">John Smith</a>
+      <nuxt-link v-bind:to="authorLink">{{ post.author_data.nickname }}</nuxt-link>
     </div>
-    <p class="post-author__desc">Lorem ipsum, dolor sit amet conse ctetur adipi sicing elit.
+      <p class="post-author__desc">Lorem ipsum, dolor sit amet conse ctetur adipi sicing elit.
       Necessitatibus, vel vero vel vero vel vero vel vero!</p>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 export default {
+  computed: {
+    ...mapState({
+      post: state => state.posts.postDetail
+    }),
+    authorLink() {
+      return `/user/${this.post.author}`;
+    },
+    getAvatar() {
+      if (this.post.author_data.avatar) {
+        return this.post.author_data.avatar;
+      }
 
+      const userId = this.post.author;
+      return `/assets/images/avatar${userId % 4 + 1}.jpg`;
+    }
+  },
 }
 </script>
 
