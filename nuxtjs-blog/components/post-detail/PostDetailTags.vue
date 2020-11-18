a2b41e1ebacb10c41f5a7c32b1ec988f7c3f1f59
vudinhquang <vudinhquangk53@gmail.com>
2020-11-18 22:57:04 +0900

    Render detail 2

--- nuxtjs-blog/components/post-detail/PostDetailTags.vue
+++ nuxtjs-blog/components/post-detail/PostDetailTags.vue
@@ -2,17 +2,48 @@
   <div class="post-detail__tags">
     <h2>Tags</h2>
     <ul>
-      <li class="item"><a href="#" class="btn btn-default">HTML</a></li>
-      <li class="item"><a href="#" class="btn btn-default">CSS3</a></li>
-      <li class="item"><a href="#" class="btn btn-default">React</a></li>
-      <li class="item"><a href="#" class="btn btn-default">Vue</a></li>
+      <li class="item" v-for="item in listTags" v-bind:key="item.id">
+        <AppButton v-bind:href="getSlug(item)" type="category">{{ item.name }}</AppButton>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    post: {
+      type: Object,
+      default: null
+    },
+  },
+  data() {
+    return {
+      listTags: []
+    }
+  },
+  methods: {
+    getSlug(item) {
+      return '/tag/' + item.slug;
+    }
+  },
+  async fetch() {
+    if (!this.post) {
+      return;
+    }
 
+    try {
+      const res = await this.$store.dispatch('tags/actFetchListTags', {
+        include: this.post.tags
+      })
+
+      if (res.ok) {
+        this.listTags = res.data;
+      }
+    } catch(e) {
+
+    }
+  }
 }
 </script>
 
