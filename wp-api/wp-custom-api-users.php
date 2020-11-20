54c49b8995cf8e9c5040ad779e8aab9db15c7cd1
vudinhquang <vudinhquangk53@gmail.com>
2020-11-21 05:11:13 +0900

    WP Register Route 3

--- wp-api/wp-custom-api-users.php
+++ wp-api/wp-custom-api-users.php
@@ -29,14 +29,51 @@
     ));
   });
 
+	function tcl_check_user_password( $value) {
+		$password = (string) $value;
+
+		if ( empty( $password ) ) {
+			return new WP_Error(
+				'rest_user_invalid_password',
+				__( 'Passwords cannot be empty.' ),
+				array( 'status' => 400 )
+			);
+		}
+
+		if ( false !== strpos( $password, '\\' ) ) {
+			return new WP_Error(
+				'rest_user_invalid_password',
+				__( 'Passwords cannot contain the "\\" character.' ),
+				array( 'status' => 400 )
+			);
+    }
+    
+		if ( false !== strpos( $password, ' ' ) ) {
+			return new WP_Error(
+				'rest_user_invalid_password',
+				__( 'Passwords cannot contain the space character.' ),
+				array( 'status' => 400 )
+			);
+		}
+
+		return $password;
+	}
+
   function handle_route_users_register($request) {
+    /*
     $status = 200;
     $data = array(
       'email' => $request->get_param('email'),
       'password' => $request->get_param('password')
     );
     $response = new WP_REST_Response( $data, $status );
+    */
 
-    return $response;
+    $email = $request->get_param('email');
+    $username = $request->get_param('username');
+    $password = tcl_check_user_password($request->get_param('password'));
+    $nickname = $request->get_param('nickname');
+
+    if($password instanceof WP_Error) return $password;
   }
 ?>
