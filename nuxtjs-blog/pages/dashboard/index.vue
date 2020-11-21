3b2d2eace3440fcd2e6d97cae28a1b26afd585bd
vudinhquang <vudinhquangk53@gmail.com>
2020-11-22 03:38:54 +0900

    Ant Form 1

--- nuxtjs-blog/pages/dashboard/index.vue
+++ nuxtjs-blog/pages/dashboard/index.vue
@@ -2,7 +2,51 @@
   <div>
     <a-row :gutter="30">
       <a-col :span="12">
-        col-12
+        <a-form
+          :form="form"
+          :label-col="{ span: 5 }"
+          :wrapper-col="{ span: 12 }"
+          @submit="handleSubmit"
+        >
+          <!-- Input -->
+          <a-form-item label="Email">
+            <a-input
+              v-decorator="[
+                'note',
+                {
+                  rules: [
+                    { required: true, message: 'Email bắt buộc phai nhập!' }
+                  ]
+                }
+              ]"
+              v-bind:disabled="true"
+            />
+          </a-form-item>
+          <!-- Select Box -->
+          <a-form-item label="Giới tính">
+            <a-select
+              v-decorator="[
+                'gender',
+                { rules: [{ required: true, message: 'Vui lòng chọn giới tính!' }] },
+              ]"
+              placeholder="Chọn giới tính"
+              @change="handleSelectChange"
+            >
+              <a-select-option value="male">
+                male
+              </a-select-option>
+              <a-select-option value="female">
+                female
+              </a-select-option>
+            </a-select>
+          </a-form-item>
+          <!-- Button Submit -->
+          <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+            <a-button type="primary" html-type="submit">
+              Submit
+            </a-button>
+          </a-form-item>
+        </a-form>
       </a-col>
       <a-col :span="12">
         col-12
@@ -14,6 +58,28 @@
 <script>
 export default {
   layout: 'admin',
+  data() {
+    return {
+      formLayout: 'horizontal',
+      form: this.$form.createForm(this, { name: 'coordinated' }),
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    },
+    handleSelectChange(value) {
+      console.log(value);
+      this.form.setFieldsValue({
+        note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+      });
+    },
+  },
 }
 </script>
 
