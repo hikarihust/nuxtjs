625b5e4674246e5d3918ef922a77c0e775ff5766
vudinhquang <vudinhquangk53@gmail.com>
2020-11-21 23:02:16 +0900

    Install Ant design

---
+++ nuxtjs-blog/layouts/admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Layout Admin</h1>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import antDesignVue from '@/plugins/ant-design-vue';
+export default {
+  beforeCreate() {
+    antDesignVue();
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ nuxtjs-blog/pages/dashboard/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Page / Dashboard > index.vue</h1>
+    <a-button type="primary">
+      Primary
+    </a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+}
+</script>
+
+<style>
+
+</style>
