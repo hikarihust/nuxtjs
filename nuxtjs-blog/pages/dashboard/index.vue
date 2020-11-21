4f1e4dfed6915ab06c340e98add688d1570456bc
vudinhquang <vudinhquangk53@gmail.com>
2020-11-22 04:00:58 +0900

    Ant Form 2

--- nuxtjs-blog/pages/dashboard/index.vue
+++ nuxtjs-blog/pages/dashboard/index.vue
@@ -4,46 +4,65 @@
       <a-col :span="12">
         <a-form
           :form="form"
-          :label-col="{ span: 5 }"
-          :wrapper-col="{ span: 12 }"
+          :label-col="{ span: 8 }"
+          :wrapper-col="{ span: 16 }"
           @submit="handleSubmit"
         >
-          <!-- Input -->
+          <!-- Email -->
           <a-form-item label="Email">
             <a-input
-              v-decorator="[
-                'note',
-                {
-                  rules: [
-                    { required: true, message: 'Email bắt buộc phai nhập!' }
-                  ]
-                }
-              ]"
+              v-decorator="['email', {
+                initialValue: 'test Email'
+              }]"
               v-bind:disabled="true"
             />
           </a-form-item>
-          <!-- Select Box -->
-          <a-form-item label="Giới tính">
-            <a-select
-              v-decorator="[
-                'gender',
-                { rules: [{ required: true, message: 'Vui lòng chọn giới tính!' }] },
-              ]"
-              placeholder="Chọn giới tính"
-              @change="handleSelectChange"
-            >
-              <a-select-option value="male">
-                male
-              </a-select-option>
-              <a-select-option value="female">
-                female
-              </a-select-option>
-            </a-select>
+
+          <!-- Username -->
+          <a-form-item label="Username">
+            <a-input
+              v-decorator="['user_name', {
+                  initialValue: 'test Username'
+              }]"
+              v-bind:disabled="true"
+            />
           </a-form-item>
+
+          <a-form-item label="Nickname">
+            <a-input
+              v-decorator="['nickname', {
+                  rules: [
+                    { required: true, message: 'Nickname bắt buộc phải nhập!' }
+                  ],
+                  initialValue: 'test nickname'
+              }]"
+            />
+          </a-form-item>
+
+          <a-form-item label="Họ và tên">
+            <a-input
+              v-decorator="['fullname', {
+                  rules: [
+                    { required: true, message: 'Fullname bắt buộc phải nhập!' }
+                  ],
+                  initialValue: 'test fullname'
+              }]"
+            />
+          </a-form-item>
+
+          <a-form-item label="Giới thiệu bản thân">
+            <a-textarea
+              v-decorator="['description', {
+                initialValue: 'test description'
+              }]"
+              v-bind:auto-size="{ minRows: 5 }"
+            />
+          </a-form-item>
+
           <!-- Button Submit -->
-          <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+          <a-form-item :wrapper-col="{ span: 16, offset: 8 }">
             <a-button type="primary" html-type="submit">
-              Submit
+              Cập nhật thông tin
             </a-button>
           </a-form-item>
         </a-form>
